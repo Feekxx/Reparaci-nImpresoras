0da18ff207fc1209e66be04610ddf6163af02eda
Feekxx <feekxx_feekxx@hotmail.com>
2023-05-17 20:48:02 -0500

    Initial commit

---
+++ LOGGIN/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","","usuarios")or die(
+    "error de conexion");
+?>
---
+++ LOGGIN/validar.php
@@ -0,0 +1,27 @@
+<?php
+include('db.php');
+$usuario=$_POST['usuario'];
+$password=$_POST['contraseña'];
+
+
+
+$consulta="SELECT*FROM credencial where usuario='$usuario' and contraseña='$password'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:home.php");
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
